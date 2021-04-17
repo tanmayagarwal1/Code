b6117610ad1f752c05745856debbb29d1a781e4a
Tanmay Agarwal <tanmay@Tanmays-MacBook-Pro.local>
2021-04-18 02:28:42 +0530

    Dynamic classes

--- Algorithms/main_test.py
+++ Algorithms/main_test.py
@@ -10,10 +10,11 @@ def email(self,sti):
 	self.first=x
 	self.last=y 
 def stri(self):
-	return """This class is a dynamic class and has the following properties : initialise 
+	return f"""This class is a dynamic class for {(self.first).upper()} and has the following properties : initialise 
 with first and last name and then use email attribute which can also be used 
 to set the name values with the . operator in middle"""
 
 A=type('A',(),{'__init__':init,'email':email,'__str__':stri})
-t=A("tanmay","Agarwal")
+t=A("shivika","garg")
+t.email="tanmay.Agarwal"
 print(t)
