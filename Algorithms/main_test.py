198899544d63b543bc517eb6aa79668fb226edf3
Tanmay Agarwal <tanmay@Tanmays-MacBook-Pro.local>
2021-04-17 10:56:19 +0530

    Fine Tunings

--- Algorithms/main_test.py
+++ Algorithms/main_test.py
@@ -18,6 +18,21 @@ def show(root):
 		show(root.right)
 	else:
 		return 
+def levelorder(root):
+	if root==None:
+		return 
+	else:
+		h=height(root)
+		for i in range(h):
+			levelorderu(root,i)
+def levelorderu(root,l):
+	if root==None:
+		return 
+	if l==0:
+		print(root.data)
+	else:
+		levelorderu(root.left,l-1)
+		levelorderu(root.right,l-1)
 def height(root):
 	if root==None:
 		return 0 
@@ -35,4 +50,4 @@ push(root,30)
 push(root,5)
 push(root,6)
 push(root,4)
-print(height(root))
+levelorder(root)
