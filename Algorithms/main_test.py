a15623bc97a554c0de36a90a676b6bc08ef82c2c
Tanmay Agarwal <tanmay@Tanmays-MacBook-Pro.local>
2021-04-17 14:25:16 +0530

    This is adding a new funnction

--- Algorithms/main_test.py
+++ Algorithms/main_test.py
@@ -1,53 +1,68 @@
 class node:
 	def __init__(self,data):
 		self.data=data
-		self.left=self.right=None 
-def push(root,data):
-	if root==None:
-		return node(data)
-	else:
-		if root.data<data:
-			root.right=push(root.right,data)
-		elif root.data>data:
-			root.left=push(root.left,data)
-		return root
-def show(root):
-	if root:
-		show(root.left)
-		print(root.data)
-		show(root.right)
-	else:
-		return 
-def levelorder(root):
-	if root==None:
-		return 
-	else:
-		h=height(root)
-		for i in range(h):
-			levelorderu(root,i)
-def levelorderu(root,l):
-	if root==None:
-		return 
-	if l==0:
-		print(root.data)
-	else:
-		levelorderu(root.left,l-1)
-		levelorderu(root.right,l-1)
-def height(root):
-	if root==None:
-		return 0 
-	else:
-		l=height(root.left)
-		r=height(root.right)
-		if l<r:
-			return r+1
+		self.next=None
+class ll:
+	def __init__(self):
+		self.head=None 
+	def append(self,data):
+		if self.head==None:
+			self.head=node(data)
 		else:
-			return l+1
+			nn=node(data)
+			temp=self.head 
+			while temp.next != None:
+				temp=temp.next
+			temp.next=nn
+			nn.next=None
+	def view(self):
+		temp=self.head 
+		while temp != None:
+			print(temp.data)
+			temp=temp.next 
+	def len(self):
+		count=0 
+		temp=self.head 
+		while temp != None:
+			count+=1
+			temp=temp.next 
+		return count 
+	def remdup(self):
+		head=self.head 
+		while head.next!=None:
+			temp=head.next 
+			pre=head 
+			while temp != None:
+				if temp.data==head.data:
+					pre.next=temp.next
+					temp=temp.next 
+					pre=temp 
+				else:
+					pre=temp 
+					temp=temp.next 
+			head=head.next
+	def oddeve(self):
+		head=self.head 
+		temp=head.next 
+		pre=temp 
+		n=self.len()
+		while temp.next != None:
+			head.next=temp.next 
+			head=temp 
+			temp=temp.next 
+		if n%2==0:
+			head.next=pre 
+			temp.next=None
+		else:
+			head.next=None
+			temp.next=pre 
+l=ll()
+l.append(10)
+l.append(20)
+l.append(30)
+l.append(40)
+l.append(10)
+l.append(60)
+l.remdup()
+l.view()
 
-root=node(10)
-push(root,20)
-push(root,30)
-push(root,5)
-push(root,6)
-push(root,4)
-levelorder(root)
