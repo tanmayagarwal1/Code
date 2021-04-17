b5f8962eea1aa6c403de5c01963d2dfb3b411634
Tanmay Agarwal <tanmay@Tanmays-MacBook-Pro.local>
2021-04-18 02:23:17 +0530

    Added dynamic class construction

--- Algorithms/main_test.py
+++ Algorithms/main_test.py
@@ -1,77 +1,19 @@
-class node:
-	def __init__(self,data):
-		self.data=data
-		self.next=None
-class ll:
-	def __init__(self):
-		self.head=None
-	def append(self,data):
-		if self.head==None:
-			self.head=node(data)
-		else:
-			temp=self.head
-			nn=node(data)
-			while temp.next != None:
-				temp=temp.next 
-			temp.next=nn
-			nn.next=None
-	def view(self):
-		temp=self.head 
-		while temp!=None:
-			print(temp.data)
-			temp=temp.next 
-	def remdup(self):
-		head=self.head 
-		while head.next != None:
-			temp=head.next 
-			pre=temp
-			while temp != None:
-				if temp.data==head.data:
-					pre.next=temp.next 
-					temp=temp.next
-					pre=temp 
-				else:
-					pre=temp 
-					temp=temp.next
-			head=head.next 
-	def len(self):
-		temp=self.head 
-		count=0 
-		while temp != None:
-			count+=1
-			temp=temp.next 
-		return count 
+def init(self,x,y):
+	self.first=x 
+	self.last=y
+@property 
+def email(self):
+	return self.first+self.last+"@gmail.com"
+@email.setter
+def email(self,sti):
+	x,y=sti.split('.')
+	self.first=x
+	self.last=y 
+def stri(self):
+	return """This class is a dynamic class and has the following properties : initialise 
+with first and last name and then use email attribute which can also be used 
+to set the name values with the . operator in middle"""
 
-	def oddeve(self):
-		head=self.head 
-		n=self.len()
-		temp=head.next 
-		pre=temp 
-		while temp.next !=None:
-			head.next=temp.next 
-			head=temp
-			temp=temp.next 
-		if n%2==0:
-			head.next=pre 
-			temp.next=None
-		else:
-			temp.next=pre
-			head.next=None
-
-l=ll()
-l.append(10)
-l.append(20)
-l.append(10)
-l.append(30)
-l.append(50)
-l.append(60)
-l.remdup()
-l.view()
-
-
-
-
-
-
-
-	
+A=type('A',(),{'__init__':init,'email':email,'__str__':stri})
+t=A("tanmay","Agarwal")
+print(t)
