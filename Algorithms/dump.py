dcbaf11318a70637923b2778854ddcab845dfd5a
Tanmay Agarwal <tanmay@Tanmays-MacBook-Pro.local>
2021-04-19 17:50:57 +0530

    new updations

--- Algorithms/dump.py
+++ Algorithms/dump.py
@@ -1,37 +1,15 @@
-from collections import defaultdict
-def ini(self):
-	self.graph=defaultdict(list)
-def append(self,s,d):
-	self.graph[s].append(d)
-def bfs(self,v):
+def paranthesis(st):
 	q=[]
-	visited=[False]*100 
-	visited[v]=True 
-	q.append(v)
-	while q:
-		s=q.pop()
-		print(s)
-		for i in self.graph[s]:
-			if visited[i]==False:
-				q.append(i)
-				visited[i]==True
- 				
-def dfs(self,v):
-	s=set()
-	self.dfsu(v,s)
-def dfsu(self,v,s):
-	s.add(v)
-	print(v)
-	for i in self.graph[v]:
-		if i not in s:
-			self.dfsu(i,s)
-graph=type('graph',(),{"__init__":ini,'append':append,'bfs':bfs,'dfs':dfs,'dfsu':dfsu})
-g=graph()
-g.append(0,1)
-g.append(1,2)
-g.append(1,3)
-g.append(4,5)
-g.append(3,4)
-g.bfs(0)
-print()
-g.dfs(0)
+	v={'(':')','{':'}','[':']'}
+	open_br=set(['{','(','['])
+	for i in st:
+		if i in open_br:
+			q.append(i)
+		else:
+			if i==v[q[-1]]:
+				q.pop()
+			else:
+				return False
+	return True 
+
+print(paranthesis('{()}'))
--- Algorithms/egg_problem.py
+++ Algorithms/egg_problem.py
@@ -1,8 +1,9 @@
-def superEggDrop(K, N):
+def superEggDrop(K, N): ##k= eggs, n=floors
         dp = [[0] * (K + 1) for i in range(N + 1)]
         for m in range(1, N + 1):
             for k in range(1, K + 1):
                 dp[m][k] = dp[m - 1][k - 1] + dp[m - 1][k] + 1
             if dp[m][K] >= N: return m
+print(superEggDrop(2,100))
 
 
