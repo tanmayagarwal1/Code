46f883c5722a31d6ffc5abb34fee98d54930408a
Tanmay Agarwal <tanmay@Tanmays-MacBook-Pro.local>
2021-04-20 16:45:01 +0530

    In progress

--- Algorithms/dump.py
+++ Algorithms/dump.py
@@ -1,83 +1,32 @@
-def ini(self,data):
-	self.data=data
-	self.left=self.right=None
-node=type('node',(),{'__init__':ini})
-
-def push(root,data):
-	if root==None:
-		return node(data)
+from collections import defaultdict
+def ini(self,v):
+	self.graph=defaultdict(list)
+	self.v=v
+def append(self,s,d):
+	self.graph[s].append(d)
+def maxpath(self,v,d):
+	visited=[False]*(self.v)
+	q=[]
+	self.maxpathu(v,d,q,visited)	
+def maxpathu(self,v,d,q,visited):
+	visited[v]=True 
+	q.append(v)
+	if v==d:
+		print(q) 
 	else:
-		if root.data<data:
-			root.right=push(root.right,data)
-		elif root.data>data:
-			root.left=push(root.left,data)
-		return root 
-def view(root):
-	if root:
-		view(root.left)
-		print(root.data)
-		view(root.right)
-	else:
-		return 
-def height(root):
-	if root==None:
-		return 0 
-	else:
-		l=height(root.left)
-		r=height(root.right)
-		if l<r:
-			return r+1
-		else:
-			return l+1 
-def levelorder(root):
-	if root==None:
-		return 
-	else:
-		h=height(root)
-		q=[]
-		for i in range(h):
-			levelorderu(root,i,q)
-		return q 
-def levelorderu(root,l,q):
-	if root==None:
-		return 
-	if l==0:
-		q.append(root.data)
-	else:
-		levelorderu(root.left,l-1,q)
-		levelorderu(root.right,l-1,q)
-
-class ll:
-	def __init__(self):
-		self.head=None
-	def append(self,data):
-		if self.head==None:
-			self.head=node(data)
-		else:
-			temp=self.head
-			n=node(data)
-			while temp.right != None:
-				temp=temp.right 
-			temp.right=n
-			n.left=temp 
-			n.right=None
-	def make(self,q):
-		for i in q:
-			self.append(i)
-	def show(self):
-		temp=self.head 
-		while temp != None:
-			print(temp.data)
-			temp=temp.right
+		for i in self.graph[v]:
+			if visited[i]==False:
+				self.maxpathu(i,d,q,visited)
+	q.pop()
+	visited[v]=False
+graph=type('graph',(),{'__init__':ini,'append':append,'maxpath':maxpath,'maxpathu':maxpathu})
+g = graph(4)
+g.append(0, 1)
+g.append(0, 2)
+g.append(0, 3)
+g.append(2, 0)
+g.append(2, 1)
+g.append(1, 3)
+print(g.maxpath(2,3))
 
 
-root=node(10)
-push(root,11)
-push(root,12)
-push(root,9)
-push(root,8)
-
-
-l=ll()
-l.make(x)
-l.show()
